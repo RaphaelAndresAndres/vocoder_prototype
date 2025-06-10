b962ab6e0aa437d91a0ee0ff78ae035b0f41b401
Raphael Andres <raphael.andres@student.tugraz.at>
2025-06-10 21:46:30 +0200

    make graph work in master branch

--- GLMethods.c
+++ GLMethods.c
@@ -84,9 +84,10 @@ void draw(float time, float *source)
 
     for (int i = 0; i < numPoints; ++i)
     {
-        float x = log((float)i / (float)numPoints) * 2 * limits - limits;
+        float t = log((float)i + 1) / logf(numPoints + 1);
+        float x = t * 2 * limits - limits;
         waveVertices[2 * i] = x;
-        waveVertices[2 * i + 1] = source[i] / FRAMES_PER_BUFFER;
+        waveVertices[2 * i + 1] = source[i] / (float)FRAMES_PER_BUFFER;
     }
 
     GLuint VBO[numElements], VAO[numElements];
